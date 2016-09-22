f560a90b89c869a1a42ab9ff55263e72af2fe927
nicoDigiFors <nico.mueller@digifors.de>
2016-09-22 15:12:44 +0200

    Add files via upload

--- adb_ex.sh
+++ adb_ex.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
-#@(#) AUTHOR='N.Mueller,nico.mueller@digifors.de, phone: 0341.24146870'
-#@(#) VERSION=0.1
-#@(#) DATE=19-09.2016
-#@(#) File: abe_ex.sh
-#@(#) Parameter: [-i|-?|-h|-help]
-##
-## Last Modified By:
-##      nico.mueller@digifors.de
-##      Phone: 0341.24146870
-##      DigiFors GmbH
-##      Schongauer Str. 29-31
-##      04328 Leipzig
-##
+#
+#
+#-------------------------------------------------------------------------------
+#
+# MIT License
+#
+# Copyright (c) 2016 DigiFors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# ------------------------------------------------------------------------------
 
 AUTHOR='N.Mueller,nico.mueller@digifors.de, phone: 0341.24146870'
 VERSION=0.1
